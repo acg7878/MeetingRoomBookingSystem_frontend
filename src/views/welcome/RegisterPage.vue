00359dceee21196530e5d1295ece478eb70768b5
macbook_air <2477952281@qq.com>
2025-05-07 09:20:49 +0800

    feat: 添加用户角色选择和优化注册表单样式

--- src/views/welcome/RegisterPage.vue
+++ src/views/welcome/RegisterPage.vue
@@ -1,63 +1,115 @@
 <template>
-  <div style="text-align: center;margin: 0 20px">
+  <div style="text-align: center; margin: 0 20px">
     <div style="margin-top: 100px">
-      <div style="font-size: 25px;font-weight: bold">注册新用户</div>
-      <div style="font-size: 14px;color: grey">欢迎注册会议室预订系统，请在下方填写相关信息</div>
+      <div style="font-size: 25px; font-weight: bold">注册新用户</div>
+      <div style="font-size: 14px; color: grey">
+        欢迎注册会议室预订系统，请在下方填写相关信息
+      </div>
     </div>
     <div style="margin-top: 50px">
-      <el-form :model="form" :rules="rules" @validate="onValidate" ref="formRef">
+      <el-form
+        :model="form"
+        :rules="rules"
+        @validate="onValidate"
+        ref="formRef"
+      >
         <el-form-item prop="username">
-          <el-input v-model="form.username" :maxlength="8" type="text" placeholder="用户名">
+          <el-input
+            v-model="form.username"
+            :maxlength="8"
+            type="text"
+            placeholder="用户名"
+          >
             <template #prefix>
               <el-icon><User /></el-icon>
             </template>
           </el-input>
         </el-form-item>
         <el-form-item prop="password">
-          <el-input v-model="form.password" :maxlength="16" type="password" placeholder="密码">
+          <el-input
+            v-model="form.password"
+            :maxlength="16"
+            type="password"
+            placeholder="密码"
+          >
             <template #prefix>
               <el-icon><Lock /></el-icon>
             </template>
           </el-input>
         </el-form-item>
         <el-form-item prop="password_repeat">
-          <el-input v-model="form.password_repeat" :maxlength="16" type="password" placeholder="重复密码">
+          <el-input
+            v-model="form.password_repeat"
+            :maxlength="16"
+            type="password"
+            placeholder="重复密码"
+          >
             <template #prefix>
               <el-icon><Lock /></el-icon>
             </template>
           </el-input>
         </el-form-item>
         <el-form-item prop="email">
-          <el-input v-model="form.email" type="email" placeholder="电子邮件地址">
+          <el-input
+            v-model="form.email"
+            type="email"
+            placeholder="电子邮件地址"
+          >
             <template #prefix>
               <el-icon><Message /></el-icon>
             </template>
           </el-input>
         </el-form-item>
+        <el-form-item prop="role">
+          <el-select v-model="form.role" placeholder="请选择用户类型">
+            <el-option label="管理员" value="admin"></el-option>
+            <el-option label="客户" value="customer"></el-option>
+            <el-option label="员工" value="employee"></el-option>
+          </el-select>
+        </el-form-item>
         <el-form-item prop="code">
           <el-row :gutter="10" style="width: 100%">
             <el-col :span="17">
-              <el-input v-model="form.code" :maxlength="6" type="text" placeholder="请输入验证码">
+              <el-input
+                v-model="form.code"
+                :maxlength="6"
+                type="text"
+                placeholder="请输入验证码"
+              >
                 <template #prefix>
                   <el-icon><EditPen /></el-icon>
                 </template>
               </el-input>
             </el-col>
             <el-col :span="5">
-              <el-button type="success" @click="validateEmail" :disabled="!isEmailValid || coldTime > 0">
-                {{ coldTime > 0 ? '请稍后 ' + coldTime + ' 秒' : '获取验证码' }}
+              <el-button
+                type="success"
+                @click="validateEmail"
+                :disabled="!isEmailValid || coldTime > 0"
+              >
+                {{ coldTime > 0 ? "请稍后 " + coldTime + " 秒" : "获取验证码" }}
               </el-button>
             </el-col>
           </el-row>
         </el-form-item>
+        
       </el-form>
     </div>
     <div style="margin-top: 80px">
-      <el-button style="width: 270px" type="warning" @click="register" plain>立即注册</el-button>
+      <el-button style="width: 270px" type="warning" @click="register" plain
+        >立即注册</el-button
+      >
     </div>
     <div style="margin-top: 20px">
-      <span style="font-size: 14px;line-height: 15px;color: grey">已有账号? </span>
-      <el-link type="primary" style="translate: 0 -2px" @click="router.push('/')">立即登录</el-link>
+      <span style="font-size: 14px; line-height: 15px; color: grey"
+        >已有账号?
+      </span>
+      <el-link
+        type="primary"
+        style="translate: 0 -2px"
+        @click="router.push('/')"
+        >立即登录</el-link
+      >
     </div>
   </div>
 </template>
@@ -76,6 +128,7 @@ interface Form {
   password_repeat: string;
   email: string;
   code: string;
+  role: string;
 }
 
 // 表单数据
@@ -85,10 +138,15 @@ const form = reactive<Form>({
   password_repeat: "",
   email: "",
   code: "",
+  role: "",
 });
 
 // 表单验证规则
-const validateUsername = (rule: any, value: string, callback: (error?: Error) => void) => {
+const validateUsername = (
+  rule: any,
+  value: string,
+  callback: (error?: Error) => void
+) => {
   if (value === "") {
     callback(new Error("请输入用户名"));
   } else if (!/^[a-zA-Z0-9\u4e00-\u9fa5]+$/.test(value)) {
@@ -98,7 +156,11 @@ const validateUsername = (rule: any, value: string, callback: (error?: Error) =>
   }
 };
 
-const validatePassword = (rule: any, value: string, callback: (error?: Error) => void) => {
+const validatePassword = (
+  rule: any,
+  value: string,
+  callback: (error?: Error) => void
+) => {
   if (value === "") {
     callback(new Error("请再次输入密码"));
   } else if (value !== form.password) {
@@ -111,18 +173,35 @@ const validatePassword = (rule: any, value: string, callback: (error?: Error) =>
 const rules = {
   username: [
     { validator: validateUsername, trigger: ["blur", "change"] },
-    { min: 2, max: 8, message: "用户名的长度必须在2-8个字符之间", trigger: ["blur", "change"] },
+    {
+      min: 2,
+      max: 8,
+      message: "用户名的长度必须在2-8个字符之间",
+      trigger: ["blur", "change"],
+    },
   ],
   password: [
     { required: true, message: "请输入密码", trigger: "blur" },
-    { min: 6, max: 16, message: "密码的长度必须在6-16个字符之间", trigger: ["blur", "change"] },
+    {
+      min: 6,
+      max: 16,
+      message: "密码的长度必须在6-16个字符之间",
+      trigger: ["blur", "change"],
+    },
   ],
-  password_repeat: [{ validator: validatePassword, trigger: ["blur", "change"] }],
+  password_repeat: [
+    { validator: validatePassword, trigger: ["blur", "change"] },
+  ],
   email: [
     { required: true, message: "请输入邮件地址", trigger: "blur" },
-    { type: "email", message: "请输入合法的电子邮件地址", trigger: ["blur", "change"] },
+    {
+      type: "email",
+      message: "请输入合法的电子邮件地址",
+      trigger: ["blur", "change"],
+    },
   ],
   code: [{ required: true, message: "请输入获取的验证码", trigger: "blur" }],
+  role: [{ required: true, message: "请选择用户类型", trigger: "change" }],
 };
 
 // 引用和状态
@@ -146,9 +225,10 @@ const register = () => {
           password: form.password,
           email: form.email,
           code: form.code,
+          role: form.role,
         },
         () => {
-          ElMessage.success("注册成功，欢迎加入我们");
+          ElMessage.success("注册成功，欢迎使用本系统");
           router.push("/");
         }
       );
@@ -178,9 +258,6 @@ const validateEmail = () => {
     }
   );
 };
-
-
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
