675f0fbc4633021d4221ca9d54bbd5e286225264
macbook_air <2477952281@qq.com>
2025-05-08 12:40:35 +0800

    refactor: 引入Pinia并注册Element Plus图标组件

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import { createApp } from "vue";
+import { createPinia } from 'pinia';
+import App from "./App.vue";
+import router from "./router";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import "./style/index.scss"
 
-const app = createApp(App)
-app.use(router)
-app.use(ElementPlus)
-
-app.mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+app.use(router);
+app.use(ElementPlus);
+app.use(pinia);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+app.mount("#app");
