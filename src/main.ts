73d6124096c359545c8528dfa6800f9dd353057b
macbook_air <2477952281@qq.com>
2025-05-09 12:15:05 +0800

    refactor(main): 优化Pinia导入方式，简化应用初始化

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import { createPinia } from 'pinia';
+import pinia from '@/store'
 import App from "./App.vue";
 import router from "./router";
 import ElementPlus from "element-plus";
@@ -8,10 +8,10 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import "./style/index.scss"
 
 const app = createApp(App);
-const pinia = createPinia();
+app.use(pinia);
 app.use(router);
 app.use(ElementPlus);
-app.use(pinia);
+
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
