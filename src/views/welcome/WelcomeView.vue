455bd0eafed274d7efc5750fe1431665d6b42f7a
macbook_air <2477952281@qq.com>
2025-05-20 16:21:20 +0800

    feat: 添加封面图片并优化欢迎页面布局

--- src/views/welcome/WelcomeView.vue
+++ src/views/welcome/WelcomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+  <div style="width: 100vw; height: 100vh; overflow: hidden; display: flex">
     <div style="flex: 1">
-      <el-image style="width: 100%;height: 100%" fit="cover"
-                src="https://img1.baidu.com/it/u=4097856652,4033702227&fm=253&fmt=auto&app=120&f=JPEG?w=1422&h=800"/>
+      <el-image style="width: 100%; height: 100%" fit="cover" :src="my_coverImg" />
     </div>
     <div class="welcome-title">
-      <div style="font-size: 30px;font-weight: bold">欢迎使用会议室预订系统</div>
+      <div style="font-size: 30px; font-weight: bold">
+        欢迎使用会议室预订系统
+      </div>
       <!-- <div style="margin-top: 10px">在这里你可以学习如何使用Java，如何搭建网站，并且与Java之父密切交流。</div>
       <div style="margin-top: 5px">在这里你可以同性交友，因为都是男的，没有学Java的女生。</div> -->
     </div>
     <div class="right-card">
       <router-view v-slot="{ Component }">
         <transition name="el-fade-in-linear" mode="out-in">
-          <component :is="Component" style="height: 100%"/>
+          <component :is="Component" style="height: 100%" />
         </transition>
       </router-view>
     </div>
@@ -20,7 +21,7 @@
 </template>
 
 <script setup>
-
+import my_coverImg from "@/assets/cover.jpg";
 </script>
 
 <style scoped>
